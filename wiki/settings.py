e8007afb77196e91c137c68abc5321050883f77c
meghanakolluri <kollurimeghana388@gmail.com>
2021-06-15 23:29:24 +0530

    commit1

--- wiki/settings.py
+++ wiki/settings.py
@@ -127,3 +127,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL='/static/'
 
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
