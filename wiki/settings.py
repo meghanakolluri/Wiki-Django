4c451cdd4deae5f22a3cbb772a666eeb8a8f7119
meghanakolluri <kollurimeghana388@gmail.com>
2021-06-15 23:39:58 +0530

    commit1

--- wiki/settings.py
+++ wiki/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
-STATIC_URL='/static/'
-
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
